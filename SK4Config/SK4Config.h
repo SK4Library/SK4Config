a3f4a9c55ddd36b78e4f89416736b7b5fd66e7d2
See.Ku <info@see-ku.com>
2016-12-27 14:21:56 +0900

    Initial Commit

---
+++ SK4Config/SK4Config.h
@@ -0,0 +1,19 @@
+//
+//  SK4Config.h
+//  SK4Config
+//
+//  Created by See.Ku on 2016/12/27.
+//  Copyright (c) 2016 AxeRoad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SK4Config.
+FOUNDATION_EXPORT double SK4ConfigVersionNumber;
+
+//! Project version string for SK4Config.
+FOUNDATION_EXPORT const unsigned char SK4ConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SK4Config/PublicHeader.h>
+
+
